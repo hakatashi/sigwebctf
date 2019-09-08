a5d5388e6fa018973f709fd2e16d88bddbd83938
Koki Takahashi <hakatasiloving@gmail.com>
2019-09-08 14:35:47 +0900

    Update cookies

--- headless-chrome-runner-cookie/cookies.js
+++ headless-chrome-runner-cookie/cookies.js
@@ -1,8 +1,8 @@
 const name = 'flag';
-const host = '104.28.29.116';
+const host = '3.114.12.105';
 module.exports = {
-    'http://104.28.29.116:10003': {'name': name, 'value': 'SWCTF{now_listening_sakura_sunrise}', 'domain': host},
-    'http://104.28.29.116:10004': {'name': name, 'value': 'SWCTF{now_listening_O-Ku-Ri-Mo-No_Sunday_M@STER_VERSION}', 'domain': host},
-    'http://104.28.29.116:10005': {'name': name, 'value': 'SWCTF{now_listening_ai_wo_tsutaetaidatoka}', 'domain': host},
-    'http://104.28.29.116:10006': {'name': name, 'value': 'SWCTF{now_listening_3_nen_C_gumi_kubosono_chiyoko_no_nyukaku}', 'domain': host},
+    'http://3.114.12.105:10003': {'name': name, 'value': 'SWCTF{now_listening_sakura_sunrise}', 'domain': host},
+    'http://3.114.12.105:10004': {'name': name, 'value': 'SWCTF{now_listening_O-Ku-Ri-Mo-No_Sunday_M@STER_VERSION}', 'domain': host},
+    'http://3.114.12.105:10005': {'name': name, 'value': 'SWCTF{now_listening_ai_wo_tsutaetaidatoka}', 'domain': host},
+    'http://3.114.12.105:10006': {'name': name, 'value': 'SWCTF{now_listening_3_nen_C_gumi_kubosono_chiyoko_no_nyukaku}', 'domain': host},
 }
