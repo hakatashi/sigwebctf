c34d8dbd80fc3e9aea054cecf4d2986ce81da87a
Koki Takahashi <hakatasiloving@gmail.com>
2019-09-10 02:23:41 +0900

    Add achievements-viewer

--- headless-chrome-runner-cookie/cookies.js
+++ headless-chrome-runner-cookie/cookies.js
@@ -5,4 +5,5 @@ module.exports = {
     'http://3.114.12.105:10004': {'name': name, 'value': 'SWCTF{now_listening_O-Ku-Ri-Mo-No_Sunday_M@STER_VERSION}', 'domain': host},
     'http://3.114.12.105:10005': {'name': name, 'value': 'SWCTF{now_listening_ai_wo_tsutaetaidatoka}', 'domain': host},
     'http://3.114.12.105:10006': {'name': name, 'value': 'SWCTF{now_listening_3_nen_C_gumi_kubosono_chiyoko_no_nyukaku}', 'domain': host},
+    'http://3.114.12.105:10007': {'name': name, 'value': 'SWCTF{now_listening_Maria_walking_across_the_100000000_pieces}', 'domain': host},
 }
